a6f34ccd1f99e2d172ac53313ff57280f3e31e59
Orkun M. KILIÇ <orkun.kilic@boun.edu.tr>
2021-04-16 21:36:17 +0300

    forms & dynamic urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -18,7 +18,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
-    path('products/', views.products),
-    path('customer/', views.customer),
+    path('', views.home, name="home"),
+    path('products', views.products, name='products'),
+    path('customer/<str:pk>', views.customer, name='customer'),
+    path('create_order/<str:pk>', views.createOrder, name="create_order"),
+    path('update_order/<str:pk>', views.updateOrder, name="update_order"),
+    path('delete_order/<str:pk>', views.deleteOrder, name="delete_order"),
 ]
